9c7ea211b995a7ef725dcf1d8028235100f30708
Paul-Antoine <paul-antoine.chevalier@data.gouv.fr>
2020-01-17 15:12:24 +0100

    update version with the script used for AMI IA 2

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # dashboard-ami
-script pour générer un dashboard pour analyser les candidatures de l'appel à manifestation IA
+
+Ce script permet de récupérer les données d'un questionnaire d'évaluation stocké dans un fichier au format Excel construit sur le modèle suivant `AMI-IA-Grille-notation.xlsx`. 
+
+## Structure des données
+
+Tous les fichiers Excel correspondant aux évaluations d'un même projet sont enregistrées dans un même sous dossier.
+Tous les fichiers Excel contenant des évaluations ont des noms commençant par 6 chiffres.
+
+## Lancement du script
+
+Pour lancer le script dans une console R : 
+
+    R > knitr::knit(input = "index.Rmd")
+
+Dans une console
+
+    $ Rscript -e 'knitr::knit(input = "index.Rmd")'
--- functions.R
+++ functions.R
@@ -1,6 +1,8 @@
 get_coupdecoeur <- function(file) {
-  readxl::read_excel(path = file, skip = 18, n_max = 2) %>%
+  suppressMessages(
+  readxl::read_excel(path = file, skip = 17, n_max = 2) %>%
     tricky::set_standard_names() %>%
+    dplyr::rename(x_1 = `_1`) %>%
     dplyr::select(x_1, cocher_la_case_le_cas_echeant_) %>%
     tidyr::spread(key = x_1, value = cocher_la_case_le_cas_echeant_) %>%
     tricky::set_standard_names() %>%
@@ -10,12 +12,33 @@ get_coupdecoeur <- function(file) {
       alerte = 1*(!is.na(alerte)), 
       coup_de_coeur = 1*(!is.na(coup_de_coeur))
     )
+  )
 }
-get_coupdecoeur(file = "082569-gendarmerie/82569-Gendarmerie nationale - Mathilde Bras.xlsx")
+get_coupdecoeur(file = "449453-EFS/449453-AF.xlsx")
+
+get_accompagnement <- function(file) {
+  suppressMessages(
+  readxl::read_excel(path = file, skip = 22, n_max = 2) %>%
+    tricky::set_standard_names() %>%
+    dplyr::rename(x_1 = `_1`) %>%
+    dplyr::select(x_1, cocher_la_case) %>%
+    tidyr::spread(key = x_1, value = cocher_la_case) %>%
+    tricky::set_standard_names() %>%
+    dplyr::mutate(
+      file = file,
+      slug = sub(pattern = "^([[:digit:][:alpha:]\\-]*)\\/.*", replacement = "\\1", x = file), 
+      accompagnement_scientifique = 1*(!is.na(accompagnement_scientifique)), 
+      accompagnement_technique_et_metier = 1*(!is.na(accompagnement_technique_et_metier)) 
+    )
+  )
+}
+get_accompagnement(file = "449453-EFS/449453-AF.xlsx")
 
 get_evaluateur <- function(file) {
+  suppressMessages(
   readxl::read_excel(path = file) %>%
     tricky::set_standard_names() %>%
+    dplyr::rename(x_1 = `_2`) %>%
     dplyr::mutate(
       file = file
     ) %>%
@@ -23,10 +46,12 @@ get_evaluateur <- function(file) {
       appel_a_manifestation_d_interet_pour_l_intelligence_artificielle_grille_d_analyse_des_candidatures == "Evaluateur :"
     ) %>%
     dplyr::select(file, evaluateur = x_1)
+  )
 }
-get_evaluateur("082569-gendarmerie/82569-Gendarmerie nationale - Mathilde Bras.xlsx")
+get_evaluateur(file = "449453-EFS/449453-AF.xlsx")
 
 get_notes <- function(file) {
+  suppressMessages(
   readxl::read_excel(path = file, skip = 6) %>%
     tricky::set_standard_names() %>%
     dplyr::mutate(
@@ -36,8 +61,9 @@ get_notes <- function(file) {
     ) %>%
     dplyr::filter(grepl(pattern = "^[[:digit:]].*", x = critere)) %>%
     dplyr::select(slug, file, critere, note_entre_1_et_5_, commentaires)
+    )
   }  
-get_notes(file = "082569-gendarmerie/82569-Gendarmerie nationale - Mathilde Bras.xlsx")
+get_notes(file = "449453-EFS/449453-AF.xlsx")
 
 get_review <- function(file) {
   get_notes(file = file) %>%
